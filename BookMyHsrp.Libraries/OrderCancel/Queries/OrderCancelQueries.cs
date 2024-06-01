d1f05736d41cb006e79a4b17ca9f90c27efec132
YashwantBisht1999 <yashwant.singh@rosmertatech.com>
2024-06-01 10:02:52 +0530

    update ordercancel

--- BookMyHsrp.Libraries/OrderCancel/Queries/OrderCancelQueries.cs
+++ BookMyHsrp.Libraries/OrderCancel/Queries/OrderCancelQueries.cs
@@ -10,6 +10,8 @@ namespace BookMyHsrp.Libraries.OrderCancel.Queries
     {
         public static string DealerWalletdetail = "select * from[HSRPOEM].dbo.dealerwalletorder where OrderNo = @OrderNo and VehicleRegNo = @VehicleregNo";
         public static string CancelOrderGet = "exec usp_CancelOrderGet @OrderNo,@VehicleregNo";
+        public static string  CancelOrderDetails = "exec usp_Appointment_BookingHist_CencelOrder @OrderNo, @VehicleregNo" ;
+        public static string DealerAddress = "select DealerAffixationCenterAddress,DealerAffixationCenterName,DealerAffixationCenterCity,DealerAffixationCenterPinCode from [HSRPOEM].dbo.DealerAffixationCenter where DealerID = @DealerId ";
     }
 }
 
--- BookMyHsrp/Controllers/CommonController/OrderCancelController.cs
+++ BookMyHsrp/Controllers/CommonController/OrderCancelController.cs
@@ -1,18 +1,38 @@
-﻿using BookMyHsrp.Libraries.OrderCancel.Models;
+﻿using BookMyHsrp.Libraries.GenerateOtp.Services;
+using BookMyHsrp.Libraries.OrderCancel.Models;
 using BookMyHsrp.Libraries.OrderCancel.Services;
 using BookMyHsrp.Libraries.ReceiptValidity.Services;
 using BookMyHsrp.Libraries.TrackYoutOrder.Models;
 using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using System.Reflection;
 using static BookMyHsrp.Libraries.HsrpWithColorSticker.Models.HsrpColorStickerModel;
+using static BookMyHsrp.Libraries.Sticker.Models.StickerModel;
 
 namespace BookMyHsrp.Controllers.CommonController
 {
-    public class OrderCancelController(IOrderCancelServices orderCancelServices) : Controller
+    public class OrderCancelController : Controller
     {
-         
-        private readonly IOrderCancelServices _orderCancelService = orderCancelServices ?? throw new ArgumentNullException(nameof(OrderCancelServices));
 
-        [Route("/ordercancel/OrderCancel")]
+        private readonly IGenerateOtpService _generateOtpService;
+        private readonly IOrderCancelServices _orderCancelServices;
+
+        public OrderCancelController(IGenerateOtpService generateOtpService, IOrderCancelServices orderCancelServices)
+        {
+            _generateOtpService = generateOtpService;
+            _orderCancelServices = orderCancelServices;
+        }
+
+        [Route("/ordercancelOTP")]
+        [HttpPost]
+        public async Task<IActionResult> GenerateOtpOrderCancel([FromBody] OrderCancelModel.OtpModal requestdto)
+        {
+            var mobile = requestdto.MobileNo;
+            var resultGot = await _generateOtpService.GenerateOtp(mobile, requestdto);
+            return resultGot;
+        }
+
+        [Route("/ordercancel")]
         public IActionResult OrderCancel()
         {
             return View();
@@ -21,10 +41,46 @@ namespace BookMyHsrp.Controllers.CommonController
         [HttpPost]
         public async Task<IActionResult> DealerName([FromBody] OrderCancelModel.OrderCancel  requestdto)
         {
-            var response = new ResponseDto();
-            var result = await _orderCancelService.DealerWalletdetail(requestdto);
+            var result = await _orderCancelServices.DealerWalletdetail(requestdto);
 
-            if (result != null)
+            if (result.Count > 0 && result != null)
+            {
+                var input = new
+                {
+                    isAbleToCancelled = "N",
+                    ORDER_NUMBER = requestdto.OrderNo,
+                    REG_NUMBER = requestdto.VehicleregNo,
+                    MOB_NUMBER = "",
+                    OrderStatus = "Success",
+                    Emailid = ""
+                };
+                return Ok(input);
+            }
+            else
+            {
+                var cancelorderget = await _orderCancelServices.CancelOrderGet(requestdto);
+                if (cancelorderget.Count > 0)
+                {
+                    return Ok(cancelorderget);
+                }
+                else
+                {
+
+                    throw new Exception("data not found");
+
+                }
+
+            }
+
+        }
+     
+        [Route("/DealerAddress")]
+        [HttpPost]
+        public async Task<IActionResult> CancelOrderDetails([FromBody] OrderCancelModel.OrderCancel requestdto)
+        {
+            var result = await _orderCancelServices.DealerAddress(requestdto);
+            // var result = await _trackYourOrderService.GetTrackYourOrderStatusSp(requestdto)
+            if (result.Count > 0)
             {
                 return Ok(result);
             }
@@ -36,13 +92,14 @@ namespace BookMyHsrp.Controllers.CommonController
             }
 
         }
-        [Route("/CancelOrderGet")]
+    
+
+        [Route("/CancelOrderDetails")]
         [HttpPost]
         public async Task<IActionResult> CancelOrderGet([FromBody] OrderCancelModel.OrderCancel requestdto)
         {
-            var response = new ResponseDto();
-            var result = await _orderCancelService.CancelOrderGet(requestdto);
-            // var result1 = await _trackYourOrderService.GetTrackYourOrderStatusSp(requestdto)
+            var result = await _orderCancelServices.CancelOrderDetails(requestdto);
+            // var result = await _trackYourOrderService.GetTrackYourOrderStatusSp(requestdto)
             if (result != null)
             {
                 return Ok(result);
